0f4db8c1b1005a6eb008b687b4371514be763772
CraytonMacholan <24macholancrayton@blairschools.org>
2024-04-10 11:21:09 -0500

    Initial Code from Example.

---
+++ src/main/java/com/example/craytonmacholanassignment42/Image.java
@@ -0,0 +1,33 @@
+package com.example.craytonmacholanassignment42;
+
+import java.util.Arrays;
+
+public class Image {
+    private String name;
+
+    private byte[] contents;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContents() {
+        return contents;
+    }
+
+    public void setContents(byte[] contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "name='" + name + '\'' +
+                ", contents=" + Arrays.toString(contents) +
+                '}';
+    }
+}
